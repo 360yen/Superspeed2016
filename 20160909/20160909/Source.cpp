cdb3ef2b43e1d42f6ccd45b195957baa438da2e3
haruhi <haruhi@DESKTOP-8LI4NS1>
2016-09-09 08:41:28 +0900

    途中経過

--- 20160909/20160909/Source.cpp
+++ 20160909/20160909/Source.cpp
@@ -1,7 +1,18 @@
 #include<stdio.h>
 int main()
 {
-	printf("生チョコレート\n");
-	return(0);
 
+	int kihon ,a;
+		//hako02, hako03, hako04, hako05, hako06, hako07, hako08, hako09, hako10;
+	
+	printf("基本料金（１０時間まで）を入力してください\n");
+		scanf_s("%d", &kihon);
+		printf("１０時間を超えた場合の１時間の料金を入力してください\n");
+		scanf_s("%d", &a);
+	
+	
+	printf("基本料金は[\\%d]です\n",kihon);
+	printf("１時間の料金は[\\%d]です\n", a);
+	
+	return(0);
 }
